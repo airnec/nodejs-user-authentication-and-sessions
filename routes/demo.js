98bebe47bb397c5b2db67814c25bc7d4e4a16b23
Necip Dirik <necipdirik@gmail.com>
2024-07-01 09:26:24 +0300

    Validation for Login Page like what is in the  Signup Page added

--- routes/demo.js
+++ routes/demo.js
@@ -5,7 +5,6 @@ const bcrypt = require('bcryptjs');
 
 const db = require('../data/database');
 
-
 const router = express.Router();
 
 router.get('/', function (req, res) {
@@ -30,7 +29,19 @@ router.get('/signup', function (req, res) {
 });
 
 router.get('/login', function (req, res) {
-  res.render('login');
+  let sessionInputData = req.session.inputData;
+
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: '',
+      password: '',
+    };
+  }
+
+  req.session.inputData = null;
+
+  res.render('login', { inputData: sessionInputData });
 });
 
 router.post('/signup', async function (req, res) {
@@ -68,8 +79,17 @@ router.post('/signup', async function (req, res) {
     .findOne({ email: enteredEmail });
 
   if (existingUser) {
-    console.log('User exists already');
-    return res.redirect('/signup');
+    req.session.inputData = {
+      hasError: true,
+      message: 'User exists already!',
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword,
+    };
+    req.session.save(function () {
+      res.redirect('/signup');
+    });
+    return;
   }
 
   const hashedPassword = await bcrypt.hash(enteredPassword, 12);
@@ -95,8 +115,16 @@ router.post('/login', async function (req, res) {
     .findOne({ email: enteredEmail });
 
   if (!existingUser) {
-    console.log('Could not log in!');
-    return res.redirect('/login');
+    req.session.inputData = {
+      hasError: true,
+      message: 'Could not log you in - Please check your credentials!',
+      email: enteredEmail,
+      password: enteredPassword,
+    };
+    req.session.save(function () {
+      res.redirect('/login');
+    });
+    return;
   }
 
   const passwordsAreEqual = await bcrypt.compare(
@@ -105,8 +133,17 @@ router.post('/login', async function (req, res) {
   );
 
   if (!passwordsAreEqual) {
-    console.log('Could not log in - passwords are not equal!');
-    return res.redirect('/login');
+    req.session.inputData = {
+      hasError: true,
+      message: 'Invalid input - please check your data.',
+      email: enteredEmail,
+      password: enteredPassword,
+    };
+
+    req.session.save(function () {
+      res.redirect('/login');
+    });
+    return;
   }
 
   req.session.user = {
